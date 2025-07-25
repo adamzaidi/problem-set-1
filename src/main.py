cb5389f98a1cbe6bcaef0af2ec33b1b1070ce856
Adam Zaidi <93174158+adamzaidi@users.noreply.github.com>
2025-07-24 23:32:42 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 '''
 You will run this problem set from main.py, so set things up accordingly
 '''
+#as a note, i edited the names on my machine to part1_etl.py, part2_preprocesing.py, etc.
 import pandas
 from part1_etl import run_etl
 from part2_preprocessing import run_preprocessing
