49f06bb8a5a450a8725a005a86f6406ca14e6a58
Adam Zaidi <93174158+adamzaidi@users.noreply.github.com>
2025-07-24 23:29:02 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,28 +1,19 @@
 '''
 You will run this problem set from main.py, so set things up accordingly
 '''
+import pandas
+from part1_etl import run_etl
+from part2_preprocessing import run_preprocessing
+from part3_logistic_regression import run_logistic_regression
+from part4_decision_tree import run_decision_tree
+from part5_calibration_plot import run_calibration_plot
 
-import pandas as pd
-import etl
-import preprocessing
-import logistic_regression
-import decision_tree
-import calibration_plot
-
-
-# Call functions / instanciate objects from the .py files
 def main():
-
-    # PART 1: Instanciate etl, saving the two datasets in `./data/`
-
-    # PART 2: Call functions/instanciate objects from preprocessing
-
-    # PART 3: Call functions/instanciate objects from logistic_regression
-
-    # PART 4: Call functions/instanciate objects from decision_tree
-
-    # PART 5: Call functions/instanciate objects from calibration_plot
-
+    run_etl()
+    df = run_preprocessing()
+    df = run_logistic_regression(df)
+    df = run_decision_tree(df)
+    run_calibration_plot(df)
 
 if __name__ == "__main__":
     main()
